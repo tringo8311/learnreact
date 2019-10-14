a0eefc3c331bb7da68b73445237e089cb2128d32
Ngo Minh Tri <tringo8311@gmail.com>
2019-10-14 18:34:23 +0700

    Child component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,95 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Data from "./Data"
+import { FaBeer, FaDiaspora } from 'react-icons/fa';
+//import { IconContext } from "react-icons";
+
+class ListItem extends React.Component {
+	render() {
+		let listData = this.props.dataArray.map((myItem) =>
+			<li>{myItem}</li>
+		);
+		return <ul>{listData}</ul>
+	}
+} 
+
+class Summary extends React.Component {
+  	render() {
+		return <div><section class="short-summary">
+			<h1 style={{ "text-align": "center" }}>{this.props.summary.fullName}</h1>
+				<div>This is my idea about my career</div>
+			</section>
+			<section class="contact">
+				<ul>
+					<li>Gender: {this.props.summary.fullName}</li>
+					<li>Phone: {this.props.summary.phone}</li>
+					<li>Email: {this.props.summary.email}</li>
+					<li>Address: {this.props.summary.address}</li>
+				</ul>
+			</section></div>
+	}
 }
 
+class PersonalSkill extends React.Component {
+  	render() {
+		return <section class="person-skill">
+			<h2>This is my skills:</h2>
+			<ListItem dataArray={Data.skills}/>
+	  	</section>
+  	}
+}
+
+class Education extends React.Component {
+	render() {
+		return <section class="education">
+			<h2>This is my education:</h2>
+			<ul>
+				<li>Graduate 2005 at ABC University with Balenchor Certificate </li>
+				<li>Graduate 2005 at ABC University with Balenchor Certificate </li>
+				<li>Graduate 2005 at ABC University with Balenchor Certificate </li>
+				<li>Graduate 2005 at ABC University with Balenchor Certificate </li>
+			</ul>
+		</section>;
+	}
+}
+
+class WorkExperience extends React.Component {
+	render() {
+		return <section class="work-experince">
+			<h2>This is my work-experince:</h2>
+			<p>Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem </p>
+			<p>Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem </p>
+			<p>Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem </p>
+			<p>Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem </p>
+		</section>
+	}
+}
+
+class Hobby extends React.Component {
+	render() {
+		return <section class="hobby">
+			<h2>This is my hobby</h2>
+			<p>Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem </p>
+			<p>Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem </p>
+			<p>Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem </p>
+			<p>Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem </p>
+		</section>
+	}
+}
+
+function App() {
+  	return (
+    <div className="App">
+		<FaBeer /> <FaDiaspora />
+      	<section style={{ margin: "0 auto", width: "600px", "text-align": "left" }}>
+			<Summary summary={Data.summary}/>
+			<PersonalSkill/>
+			<Education/>
+			<WorkExperience/>
+			<Hobby/>
+		</section>
+    </div>
+  	);
+}
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,16 @@
+export default {
+    summary: {
+        fullName: "Ngo Minh Tri",
+        gender: "Male",
+        email: "tringo8311@gmail.com",
+        phone: "0938919689",
+        address: "Quang Trung, Q. Go vap, HCM City"
+    },
+    skills: [
+        "This is my Skill 1",
+        "This is my Skill 2",
+        "This is my Skill 3",
+        "This is my Skill 4",
+        "This is my Skill 5"
+    ]
+}
