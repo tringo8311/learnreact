3028d25014584ce44db72f5a62c71e4313308a3e
Ngo Minh Tri <tringo8311@gmail.com>
2019-11-14 20:46:26 +0700

    begin with redux

--- src/App.js
+++ src/App.js
@@ -1,66 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TodoList from "./component/todolist";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    HashRouter
-} from "react-router-dom";
-
-function Home(){
-    return ( <p>{React.version}</p> )
-}
-function About() {
-    return (<p>In this example we have 3 “pages” handled by the router: a home page, an about page, and a users page. As you click around on the different</p>)
-}
+import Note from "./component/note";
 
 function App() {
     // let location = useLocation();
     // console.log(location);
     return (
-        <HashRouter>
-            <div className="App" style={{ "width": "800px", "margin": "10px auto", "padding": "20px", "background": "#c0c0c0" }}>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/todos">Todos</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-                <Switch>
-                    <Route path="/about">
-                        <About />
-                    </Route>
-                    <Route path="/todos">
-                        <TodoList/>
-                    </Route>
-                    <Route path="/todos/all">
-                        <TodoList nowshowing="ALL"/>
-                    </Route>
-                    <Route path="/todos/active">
-                        <TodoList nowshowing="ACTIVE" />
-                    </Route>
-                    <Route path="/todos/completed">
-                        <TodoList nowshowing="COMPLETED" />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-        </HashRouter>
+        <Note />
     );
     
 }
