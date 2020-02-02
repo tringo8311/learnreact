e1af9471f051ea2e30bfabf5cbaf49aedf25689c
Ngo Minh Tri <tringo8311@gmail.com>
2020-02-02 22:04:38 +0700

    submit something

--- src/App.js
+++ src/App.js
@@ -1,68 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TodoList from "./component/todolist";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    HashRouter
-} from "react-router-dom";
-
-function Home(){
-    return ( <p>{React.version}</p> )
-}
-function About() {
-    return (<p>In this example we have 3 “pages” handled by the router: a home page, an about page, and a users page. As you click around on the different</p>)
-}
+import ExampleWithFruit from "./component/todolist";
 
 function App() {
     // let location = useLocation();
     // console.log(location);
     return (
-        <HashRouter>
-            <div className="App" style={{ "width": "800px", "margin": "10px auto", "padding": "20px", "background": "#c0c0c0" }}>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/todos">Todos</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-                <Switch>
-                    <Route path="/about">
-                        <About />
-                    </Route>
-                    <Route path="/todos">
-                        <TodoList/>
-                    </Route>
-                    <Route path="/todos/all">
-                        <TodoList nowshowing="ALL"/>
-                    </Route>
-                    <Route path="/todos/active">
-                        <TodoList nowshowing="ACTIVE" />
-                    </Route>
-                    <Route path="/todos/completed">
-                        <TodoList nowshowing="COMPLETED" />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-        </HashRouter>
-    );
-    
+        <div style={{ "width": "1000px", "margin": "20px auto" }}>
+            <ExampleWithFruit />
+        </div>
+    )
 }
 
 export default App;
